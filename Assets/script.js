9ed2d4ee9e8395ba8da5336ad8c2d5a2e29b13d2
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-08-23 20:27:20 -0600

    started api call function and tested by hard coding a city to pull data to the console

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,46 @@
 // search variables
-var searchCityEl=document.getElementbyId("searchCity");
-var searchButtonEl=document.getElementById("searchButton");
+var searchCityEl=document.getElementbyId("search-city");
+// var searchButtonEl=document.getElementById("searchButton");
 
-// Current Day Variables
-var cityEl=document.getElementById("city");
+// // Current Day Variables
+// var cityEl=document.getElementById("city");
 var tempEl=document.getElementById("temp");
 var windEl=document.getElementById("wind");
 var humidityEl=document.getElementById("humidity");
-var uvEl=document.getElementById("uv");
-var todaysWeatherEl=document.getElementById("todaysWeather");
+// var uvEl=document.getElementById("uv");
+// var todaysWeatherEl=document.getElementById("todaysWeather");
 // var currentPicEl=document.getElementById("currentPic");
 
 // 5 Day Forcast Variables
-var fiveDayEl=document.getElementById("fiveDay");
+// var fiveDayEl=document.getElementById("fiveDay");
 
 // My open weather api key
-ef26f2b071916fd09c1698141822f121
-https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var apikey="ef26f2b071916fd09c1698141822f121"
+
 
 
 
 // Fetch example from class
-var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={ef26f2b071916fd09c1698141822f121}";
-var responseText = document.getElementById('response-text');
+// var testing= `${fiveDayEl}` template literal
+var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apikey}`;
+var responseText = document.getElementById('search-city');
 
 function getApi(requestUrl) {
+  console.log("here");
   fetch(requestUrl)
     .then(function (response) {
       console.log(response);
-      if (response.status === 200) {
-        responseText.textContent = response.status;
-      }
       return response.json();
-  });
+  })
+    .then(function (data) {
+    console.log(data);
+    tempEl=data.main.temp;
+    windEl=data.wind.speed;
+    humidityEl=data.main.humidity;
+    
+
+
+  })
 }
 
 getApi(requestUrl);
